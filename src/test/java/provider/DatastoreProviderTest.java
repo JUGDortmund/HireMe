b48b15071d0550fccb286c83cbc048d32dddbfd6
Lukas Eichler <leichler@maredit.de>
2015-04-16 15:08:52 +0200

    code reformat

--- src/test/java/provider/DatastoreProviderTest.java
+++ src/test/java/provider/DatastoreProviderTest.java
@@ -1,23 +1,26 @@
 package provider;
 
-import java.util.UUID;
-import ninja.NinjaTest;
 import org.junit.Test;
 import org.mongodb.morphia.Datastore;
+
+import java.util.UUID;
+
+import ninja.NinjaTest;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class DatastoreProviderTest extends NinjaTest {
 
-	@Test
-	public void testModeHasUUIDDatabase() throws Exception {
-		assertThat(UUID.fromString(getInjector().getInstance(Datastore.class).getDB().getName())).isNotNull();
-	}
+  @Test
+  public void testModeHasUUIDDatabase() throws Exception {
+    assertThat(UUID.fromString(getInjector().getInstance(Datastore.class).getDB().getName()))
+        .isNotNull();
+  }
 
-	@Test
-	public void multipleDatastoreResolvingsReturnSameDatastore() throws Exception {
-		final Datastore datastore = getInjector().getInstance(Datastore.class);
+  @Test
+  public void multipleDatastoreResolvingsReturnSameDatastore() throws Exception {
+    final Datastore datastore = getInjector().getInstance(Datastore.class);
 
-		assertThat(datastore).isSameAs(getInjector().getInstance(Datastore.class));
-	}
+    assertThat(datastore).isSameAs(getInjector().getInstance(Datastore.class));
+  }
 }
