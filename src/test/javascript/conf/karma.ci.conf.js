b8e4706718362de9d3a7bd96d26558dbe2fb6ef9
Philipp Lewe <philipp.lewe@maredit.de>
2015-04-23 15:47:26 +0200

    HIRE-39: test reformatting

--- src/test/javascript/conf/karma.ci.conf.js
+++ src/test/javascript/conf/karma.ci.conf.js
@@ -1,31 +1,26 @@
 var baseConfig = require('./karma.conf.js');
 
-module.exports = function (config) {
-    baseConfig(config);
-    updateWebjarPaths(config);
-    config.set({
-        browsers: ['PhantomJS'],
-        reporters: ['progress', 'junit'],
-        singleRun: true,
-        autoWatch: true,
-        plugins: [
-            'karma-jasmine',
-            'karma-phantomjs-launcher',
-            'karma-junit-reporter'
-        ],
-        junitReporter: {
-            outputFile: 'target/surefire-reports/js-tests.xml',
-            suite: 'js-tests'
-        }
-    });
+module.exports = function(config) {
+  baseConfig(config);
+  updateWebjarPaths(config);
+  config.set({
+    browsers: ['PhantomJS'],
+    reporters: ['progress', 'junit'],
+    singleRun: true,
+    autoWatch: true,
+    plugins: ['karma-jasmine', 'karma-phantomjs-launcher',
+        'karma-junit-reporter'],
+    junitReporter: {
+      outputFile: 'target/surefire-reports/js-tests.xml',
+      suite: 'js-tests'
+    }
+  });
 };
 
 function updateWebjarPaths(config) {
-    for (var i = 0; i < config.files.length; i++) {
-        config.files[i] = config.files[i].replace(
-            'http://localhost:8080/webjars',
-            'target/dependency/META-INF/resources/webjars'
-        )
-    }
+  for (var i = 0; i < config.files.length; i++) {
+    config.files[i] = config.files[i].replace('http://localhost:8080/webjars',
+            'target/dependency/META-INF/resources/webjars')
+  }
 
 }
