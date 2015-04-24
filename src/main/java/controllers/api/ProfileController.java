985e9901394b7430b0e41e0f6e5b65457e633069
Lukas Eichler <leichler@maredit.de>
2015-04-24 17:28:00 +0200

    HIRE-18: Added Backend Controller

---
+++ src/main/java/controllers/api/ProfileController.java
@@ -0,0 +1,35 @@
+package controllers.api;
+
+import com.google.inject.Inject;
+import com.google.inject.Singleton;
+
+import org.mongodb.morphia.Datastore;
+
+import model.Profile;
+import ninja.Result;
+import ninja.Results;
+import ninja.jaxy.GET;
+import ninja.jaxy.POST;
+import ninja.jaxy.Path;
+
+@Singleton
+@Path("/api/profile")
+public class ProfileController {
+
+  @Inject
+  private Datastore datastore;
+
+  @GET
+  @Path("")
+  public Result getProfiles() {
+    return Results.json().render(datastore.find(Profile.class).asList());
+  }
+
+  @POST
+  @Path("")
+  public Result addProfile() {
+    Profile profile = new Profile();
+    datastore.save(profile);
+    return Results.status(201).json().render(profile);
+  }
+}
--- src/main/java/model/Profile.java
+++ src/main/java/model/Profile.java
@@ -14,6 +14,10 @@ public class Profile {
   private String lastname;
   private String careerStage;
 
+  public Profile() {
+    firstname = "Klaus";
+  }
+
   public String getFirstname() {
     return firstname;
   }
