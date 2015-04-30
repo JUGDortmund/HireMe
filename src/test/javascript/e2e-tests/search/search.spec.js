a4ece924501257d6f7da6581a6c4d53aed8af2cb
Philipp Lewe <philipp.lewe@maredit.de>
2015-04-30 17:20:36 +0200

    HIRE-14, HIRE-17: dirty commit

--- src/test/javascript/e2e-tests/search/search.spec.js
+++ src/test/javascript/e2e-tests/search/search.spec.js
@@ -1,22 +1,22 @@
-//describe('search page', function () {
-//    'use strict';
-//    
-//    var initialProfiles;
-//
-//    beforeEach(function () {
-//      browser.get('/search');
-//      element(by.id('own-profile')).click();
-//      browser.sleep(300);
-//      initialProfiles = getCurrentProfileCount();
-//    });
-//    
-//    it('should show all profiles if search keyword is empty',
-//       function () {
-//          browser.get('/search');
-//          expect(initialProfiles).toBe(getCurrentProfileCount())
-//       });
-//
-//    var getCurrentProfileCount = function() {
-//      return element.all(by.className('profile')).length;
-//    }
-//});
+describe('search page', function () {
+    'use strict';
+    
+    var initialProfiles;
+
+    beforeEach(function () {
+      browser.get('/search');
+      element(by.id('own-profile')).click();
+      browser.sleep(300);
+      initialProfiles = getCurrentProfileCount();
+    });
+    
+    it('should show all profiles if search keyword is empty',
+       function () {
+          browser.get('/search');
+          expect(initialProfiles).toBe(getCurrentProfileCount())
+       });
+
+    var getCurrentProfileCount = function() {
+      return element.all(by.className('profile')).length;
+    }
+});
