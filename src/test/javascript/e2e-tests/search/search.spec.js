1c48dcaf401f3a95c858c7c55546687ce0a891df
Philipp Lewe <philipp.lewe@maredit.de>
2015-05-04 15:45:22 +0200

    HIRE-14, HIRE-17: refactored tests

--- src/test/javascript/e2e-tests/search/search.spec.js
+++ src/test/javascript/e2e-tests/search/search.spec.js
@@ -1,26 +1,26 @@
-//'use strict';
+'use strict';
 var mock = require('protractor-http-mock');
 var SearchPage = require('../pages/search.page.js');
 
 describe('search page', function() {
 
-  var initialProfiles;
   var searchPage;
 
   beforeEach(function() {
     mock(['searchProfilesMock']);
     searchPage = new SearchPage();
-    initialProfiles = searchPage.profileCount;
   });
 
   it('should show all profiles if search keyword is empty', function() {
-    expect(initialProfiles).toBe(searchPage.profileCount);
+    searchPage.searchInputField.clear();
+    searchPage.searchInputField.sendKeys("");
     expect(element(by.id('search-results-none')).isDisplayed()).toBeFalsy();
     expect(searchPage.filteredProfileCount).toBe(3);
     expect(searchPage.profileCount).toBe(3);
   });
   
   it('should not show any profile if search keyword does not match', function() {
+    searchPage.searchInputField.clear();
     searchPage.searchInputField.sendKeys("LONG_STRING_HOPEFULLY_NOT_MATCHING_ANY_PROFILES");
     expect(searchPage.filteredProfileCount).toBe(0);
     expect(searchPage.profileCount).toBe(3);
@@ -28,10 +28,22 @@ describe('search page', function() {
   });
   
   it('should show profiles containing \"Manager\" if search keyword is \"Manager\"', function() {
+    searchPage.searchInputField.clear();
     searchPage.searchInputField.sendKeys("Manager");
     expect(element(by.id('search-results-none')).isDisplayed()).toBeFalsy();
     expect(searchPage.filteredProfileCount).toBe(2);
     expect(searchPage.profileCount).toBe(3);
+    
+    searchPage.searchInputField.clear();
+    searchPage.searchInputField.sendKeys("Moritz");
+    expect(element(by.id('search-results-none')).isDisplayed()).toBeFalsy();
+    expect(searchPage.filteredProfileCount).toBe(1);
+    expect(searchPage.profileCount).toBe(3);
+  });
+  
+  it('should search for \"blub\" if search page was called with request parameter \"/search?q=blub\"', function() {
+    browser.get('/search?q=blub');
+    expect(searchPage.searchInputField.getAttribute('value')).toBe('blub');
   });
   
 });
