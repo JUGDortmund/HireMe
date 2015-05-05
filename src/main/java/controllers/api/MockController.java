47d8b355a7b9ef86444116dfe51c42166fcb3a80
Marcel Vaterott <marcel.vaterott@maredit.de>
2015-05-05 15:47:45 +0200

    HIRE-16-frontend : added MockController for Restful mocks

---
+++ src/main/java/controllers/api/MockController.java
@@ -0,0 +1,108 @@
+package controllers.api;
+
+
+import com.google.inject.Singleton;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import ninja.Result;
+import ninja.Results;
+import ninja.jaxy.DELETE;
+import ninja.jaxy.Dev;
+import ninja.jaxy.GET;
+import ninja.jaxy.POST;
+import ninja.jaxy.PUT;
+import ninja.jaxy.Path;
+
+/**
+ * The type Mock controller.
+ *
+ * Use this controller to simulate Restful Services
+ * within hireMe. Useful when backend functionality
+ * isn't provided yet and you want to test your
+ * angularJs Rest-functions.
+ *
+ * Only provided in dev-mode!
+ */
+@Singleton
+@Path("/api/mock")
+public class MockController {
+
+  Logger LOG = LoggerFactory.getLogger(MockController.class);
+
+  @Dev
+  @GET
+  @Path("")
+  public Result getMock()
+  {
+    LOG.info("[GET] Reached getMock() in MockController---> Returning statusCode \"200\"");
+    return Results.status(200).json().render("method", "getMock");
+  }
+
+  @Dev
+  @GET
+  @Path("/notfound")
+  public Result getNotFoundMock()
+  {
+    LOG.info("[GET] Reached getNotFoundMock() in MockController---> Returning statusCode \"404\"");
+    return Results.status(404).json().render("method", "getNotFoundMock");
+  }
+
+  @Dev
+  @PUT
+  @Path("")
+  public Result putMock()
+  {
+    LOG.info("[PUT] Reached putMock() in MockController---> Returning statusCode \"200\"");
+    return Results.status(200).json().render("method", "putMock");
+  }
+
+  @Dev
+  @PUT
+  @Path("/notfound")
+  public Result putNotFoundMock(){
+    LOG.info("[PUT] Reached putNotFoundMock() in MockController---> Returning statusCode \"404\"");
+    return Results.status(404).json().render("method", "putNotFoundMock");
+  }
+
+  @Dev
+  @PUT
+  @Path("/nocontent")
+  public Result putNoContentMock(){
+    LOG.info("[PUT] Reached putNoContentMock() in MockController---> Returning statusCode \"204\"");
+    return Results.status(204).json().render("method", "putNoContentMock");
+  }
+
+  @Dev
+  @POST
+  @Path("")
+  public Result postMock() {
+    LOG.info("[POST] Reached postMock() in MockController---> Returning statusCode \"201\"");
+    return Results.status(201).json().render("method", "postMock");
+  }
+
+  @Dev
+  @POST
+  @Path("")
+  public Result postNotFoundMock() {
+    LOG.info("[POST] Reached postNotFoundMock() in MockController---> Returning statusCode \"404\"");
+    return Results.status(404).json().render("method", "postNotFoundMock");
+  }
+
+  @Dev
+  @DELETE
+  @Path("")
+  public Result deleteMock() {
+    LOG.info("[DELETE] Reached deleteMock() in MockController---> Returning statusCode \"200\"");
+    return Results.status(200).json().render("method", "deleteMock");
+  }
+
+  @Dev
+  @DELETE
+  @Path("/notfound")
+  public Result deleteNotFoundMock() {
+    LOG.info("[DELETE] Reached deleteNotFoundMock() in MockController---> Returning statusCode \"404\"");
+    return Results.status(404).json().render("method", "deleteNotFoundMock");
+  }
+}
