a21556daec07f36291ef300d890afd942194aa2e
Philipp Lewe <philipp.lewe@maredit.de>
2015-06-15 17:49:09 +0200

    HIRE-19: fixed bug, where html special characters in field names broke pdf export

--- src/main/java/conf/FreemarkerConfigurationProvider.java
+++ src/main/java/conf/FreemarkerConfigurationProvider.java
@@ -2,8 +2,11 @@ package conf;
 
 import com.google.inject.Provider;
 import com.google.inject.Singleton;
+
 import freemarker.template.Configuration;
 import services.ProfileExportService;
+
+import ninja.template.TemplateEngineFreemarkerEscapedLoader;
 
 /**
  * @author Lukas Eichler
@@ -15,6 +18,9 @@ public class FreemarkerConfigurationProvider implements Provider<Configuration> 
   public Configuration get() {
     final Configuration cfg = new Configuration(Configuration.getVersion());
     cfg.setClassForTemplateLoading(getClass(), "/" + ProfileExportService.TEMPLATES_PATH + "/");
+
+    // we are going to enable html escaping by default using this template loader:
+    cfg.setTemplateLoader(new TemplateEngineFreemarkerEscapedLoader(cfg.getTemplateLoader()));
     return cfg;
   }
 }
